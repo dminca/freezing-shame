18929dcbc2f435c198e1044b0c1d265dadc122bc
Mincă Daniel Andrei <completit@users.noreply.github.com>
2014-09-10 22:12:58 +0300

    Create FunghiPizza.java

---
+++ FunghiPizza.java
@@ -0,0 +1,7 @@
+public class FunghiPizza extends Pizza{
+    public FunghiPizza(String nume, double pret){
+        ingrediente = new Ingredient[8];
+        this.nume = nume;
+        this.pret = pret;
+    }
+}
