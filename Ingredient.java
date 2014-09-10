1c320cd1a901889c3fb738632a9e4174029681d1
Mincă Daniel Andrei <completit@users.noreply.github.com>
2014-09-10 22:13:19 +0300

    Create Ingredient.java

---
+++ Ingredient.java
@@ -0,0 +1,9 @@
+public class Ingredient implements java.io.Serializable{
+    String nume;
+    double pret;
+    
+    public Ingredient(String nume, double pret){
+        this.nume = nume;
+        this.pret = pret;
+    }
+}
