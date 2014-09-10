45c6f88c1cae60326e590760024ad229b11c9b01
Mincă Daniel Andrei <completit@users.noreply.github.com>
2014-09-10 22:15:00 +0300

    Create QuatroStagionni.java

---
+++ QuatroStagionni.java
@@ -0,0 +1,7 @@
+public class QuatroStagionni extends Pizza{
+    public QuatroStagionni(String nume, double pret){
+        ingrediente = new Ingredient[10];
+        this.nume = nume;
+        this.pret = pret;
+    }
+}
